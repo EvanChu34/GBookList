d3d834736d57ba5dba2dfb2535a094d2da51dd70
Evan Chu <echu34@gmail.com>
2021-04-07 20:19:28 -0400

    more files to add on in controller and routes

---
+++ controllers/booksController.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req,res){
+
+    },
+    fineOne: function(req,res){
+        
+    },
+    save: function(req,res){
+
+    },
+    remove: function(req,res){
+
+    }
+}
