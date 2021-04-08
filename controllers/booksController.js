0b8b8471422360356d008e86a733b34cb0c1bba3
Evan Chu <echu34@gmail.com>
2021-04-08 17:24:22 -0400

    Work on controllers need to fill in

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,15 +2,30 @@ const db = require("../models");
 
 module.exports = {
     findAll: function(req,res){
-
+       db.GBooks
+        .find()
+        .sort()
+        .then()
+        .catch()
     },
     fineOne: function(req,res){
-        
+        db.GBooks
+        .find()
+        .sort()
+        .then()
+        .catch()
     },
     save: function(req,res){
-
+        db.GBooks
+        .create()
+        .then()
+        .catch()
     },
     remove: function(req,res){
-
+        db.GBooks
+        .findById()
+        .sort()
+        .then()
+        .catch()
     }
 }
