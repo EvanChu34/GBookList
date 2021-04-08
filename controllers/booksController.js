5ff2f4986caf606d7b642724d8ef3892f76b3ba4
Evan Chu <echu34@gmail.com>
2021-04-08 17:38:27 -0400

    filling in the blanks and visual change

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,29 +3,28 @@ const db = require("../models");
 module.exports = {
     findAll: function(req,res){
        db.GBooks
-        .find()
-        .sort()
-        .then()
-        .catch()
+        .find(req.query)
+        .sort({data: -1})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
     },
     fineOne: function(req,res){
         db.GBooks
-        .find()
-        .sort()
-        .then()
-        .catch()
+        .find({id: req.params.id})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
     },
     save: function(req,res){
         db.GBooks
-        .create()
-        .then()
-        .catch()
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
     },
     remove: function(req,res){
         db.GBooks
-        .findById()
-        .sort()
-        .then()
-        .catch()
+        .findById({_id: req.params.id})
+        .sort(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
     }
 }
