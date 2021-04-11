fbf8fdba0f0a07e4ca86198b2b30586aea0738bd
Evan Chu <echu34@gmail.com>
2021-04-11 15:25:46 -0400

    fixed application and server

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
